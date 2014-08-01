cf205b4c690905848e2cd52a220d329c953f7632
Micah Iriye <miriye@csumb.edu>
2014-08-01 10:40:04 -0700

    removed userCreate method and added modifyRecord method from david's mimic api

--- src/Webity/Rest/Objects/Users.php
+++ src/Webity/Rest/Objects/Users.php
@@ -3,7 +3,9 @@ namespace Webity\Rest\Objects;
 
 use Webity\Rest\Objects;
 use Webity\Rest\Application\Api;
-use Webity\Rest\Table\TableUserGroup as UserGroup;
+use Webity\Rest\Table\TableUserGroup as UserGroup; //so we don't have to do any of that nesting business ouselves
+use Joomla\Crypt\Password\Simple;
+use Joomla\Crypt\PasswordInterface;
 
 /**
  * Methods supporting a list of {Name} records.
@@ -15,6 +17,14 @@ class Users extends Objects
 		);
 	protected $agent_id = 0;
 	protected static $instances = array();
+
+	protected $users_table; //so we don't have to keep calling api over and over again
+
+	public function __construct ()
+	{
+		$this->users_table = '#__' . Api::getInstance()->get('users_table', 'oauth_users');
+		parent::__construct();
+	}
 
 	public static function getInstance($identifier = 0)
 	{
@@ -56,9 +66,10 @@ class Users extends Objects
 		// 	->where('id = '.(int)$id);
 		//replaced with the other users table
 		$query->select('a.*, ug.id AS group_key, ug.title AS group_title')
-			  ->from('#__' . Api::getInstance()->get('users_table', 'oauth_users') . ' AS a')
+			  ->from($this->users_table . ' AS a')
 			  ->join('LEFT', '#__user_usergroup_map AS ugm ON ugm.user_id = a.id')
 			  ->join('LEFT', '#__usergroups AS ug ON ug.id = ugm.group_id');
+			  //we also will need to add the organization they are a part of
 		if(is_numeric($id)) {
 			$query->where('id = ' . (int)$id);
 		} else {
@@ -94,7 +105,7 @@ class Users extends Objects
 
 		//load the users
 		$query->select('u.id, u.username')
-			  ->from('#__' . Api::getInstance()->get('users_table', 'oauth_users') .' as u');
+			  ->from($this->users_table .' as u');
 
 		$this->processSearch($query, Api::getInstance()->input->get('users', array(), 'ARRAY'));
 
@@ -109,102 +120,6 @@ class Users extends Objects
 		return $data;
 	}
 
-	// TODO: rewrite this to work
-	public function createUser($email, $name, $code='') {
-
-		// TODO: we need to make sure that at least one user group exists before moving on with creating a user (because we are using authentication)
-
-		// create user and insert base record into advantage users page
-		jimport( 'joomla.user.helper' );
-
-		$app =& Api::getInstance();
-		// because we have a class named user as well, we need to grab the user class on our own
-		require_once (JPATH_ROOT . '/libraries/joomla/database/table/user.php');
-
-		$jom_id = JUserHelper::getUserId($email);
-		if ($jom_id) {
-			$db = Api::getInstance()->getDbo();
-			if (!$db->setQuery('SELECT id FROM #__' . Api::getInstance()->get('users_table', 'oauth_users') . ' WHERE id='.$jom_id)->loadResult()) {
-				$db->setQuery('INSERT INTO #__' . Api::getInstance()->get('users_table', 'oauth_users') . ' SET id='.$jom_id)->query();
-			}
-			return $jom_id;
-		}
-
-		if (!$code) {
-			$code = JUserHelper::genRandomPassword();
-		}
-
-		// $user_data = array(
-		// 	'username' => $email,
-		// 	'email' => $email,
-		// 	'password' => $code,
-		// 	'password2' => $code,
-		// 	'name' => $name
-		// 	);
-
-		$user_data = array(
-			'userName' => $email,
-			'password' => $code,
-			'password2' => $code
-			);
-
-		$user  = new JUser;
-		$user->bind($user_data);
-		$jom_users_id = $user->save();
-
-		// if($jom_users_id) {
-		// 	$jom_users_id = $user->id;
-		// 	JUserHelper::addUserToGroup($jom_users_id, 2);
-
-		// 	$db = Api::getInstance()->getDbo();
-		// 	$base_user = new stdClass();
-		// 	$base_user->id = $jom_users_id;
-		// 	$db->insertObject('#__advantage_users', $base_user);
-
-		// 	$mailer =& JFactory::getMailer();
-		// 	// Set a sender
-		// 	$config =& JFactory::getConfig();
-		// 	$sender = array(
-		// 		$config->get( 'mailfrom' ),
-		// 		$config->get( 'fromname' ) );
-
-		// 	$mailer->setSender($sender);
-
-		// 	// Recipient
-		// 	$mailer->addRecipient($user->email);
-
-		// 	$mailer->setSubject('Your Real Estate Ally User Details!');
-
-		// 	// Set email
-		// 	$body	= '<h3>Hello '.$user->name.',</h3>';
-		// 	$body	.= '<p>You have been added as a user for Real Estate Ally.</p>';
-		// 	$body	.= '<p>This email contains your username and password to log into <a href="http://www.yourrealestateally.com">http://www.yourrealestateally.com</a></p>';
-		// 	$body	.= '<p>Username: '.$user->email.'<br />';
-		// 	$body	.= 'Password: '.$code.'</p>';
-		// 	$body 	.= '<p>This password can be changed after logging in to the system and going to your profile from the orange dropdown in the upper right of any page.</p>';
-		// 	$body	.= '<p>Please do not respond to this message as it is automatically generated and is for information purposes only.</p>';
-		// 	$mailer->isHTML(true);
-		// 	$mailer->Encoding = 'base64';
-		// 	$mailer->setBody($body);
-		// 	// Optionally add embedded image
-		// 	//$mailer->AddEmbeddedImage( JPATH_COMPONENT.DS.'assets'.DS.'logo128.jpg', 'logo_id', 'logo.jpg', 'base64', 'image/jpeg' );
-
-		// 	if (strpos($user->email, '@makethewebwork.com')) {
-		// 		$send =& $mailer->Send();
-		// 	} else {
-		// 		$send = false;
-		// 	}
-
-		// 	if ( $send !== true ) {
-		// 		$app->enqueueMessage( 'Error sending email: ' . $send->message );
-		// 	} else {
-		// 		$app->enqueueMessage( 'Mail sent' );
-		// 	}
-		// }
-
-		return $jom_users_id;
-	}
-
 	static public function checkEmail($email) {
 		$db = Api::getInstance()->getDbo();
 		$query = $db->getQuery(true);
@@ -214,13 +129,53 @@ class Users extends Objects
 		// 	->where('email LIKE '.$db->quote($email) .' OR username LIKE '.$db->quote($email));
 
 		$query->select('id')
-			->from('#__' . Api::getInstance()->get('users_table', 'oauth_users'))
+			->from($this->users_table)
 			->where('userName LIKE '.$db->quote($email) .' OR username LIKE '.$db->quote($email));
 
 		return $db->setQuery($query, 0, 1)->loadResult();
 	}
 
+	//allows us to create a user or update an existing user...
 	protected function modifyRecord($id) {
+		$api = Api::getInstance();
+		$db = $api->getDbo();
 
+		$username = $api->input->post->get('username', null, 'STRING');
+		$password = $api->input->post->get('password', null, 'STRING');
+		//we need more than just username and password. we also need to link organization and group
+
+		if (is_null($username) && !$id) {
+			throw new \Exception('Missing required field "username"', 400);
+		}
+
+		if ($id) {
+			$query = $db->getQuery(true)
+				->select('*')
+				->from($this->users_table)
+				->where('id = ' . (int)$id);
+
+			$data = $db->setQuery($query)->loadObject();
+
+			if (!$data) {
+				throw new \Exception('User not found. Update failed.', 404);
+			}
+		} else {
+			$data = new \stdClass;
+		}
+
+		$hasher = new Simple;
+
+		// only overwrite values if they change
+		$data->username = $username ? $username : $data->username;
+		$data->password = $password ? $hasher->create($password, PasswordInterface::JOOMLA) : $data->password;
+
+		if ($data->userId) {
+			$db->updateObject($this->users_table, $data, 'id');
+		} else {
+			$db->insertObject($this->users_table, $data);
+			$data->userId = $db->insertid();
+		}
+
+		return $data;
 	}
 }
